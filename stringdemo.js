70843ccf10d22c62795d2d9b2be80efef6a89a7a
NoorainBhure <noorainbhure123@gmail.com>
2025-12-19 15:06:03 +0530

    update

---
+++ stringdemo.js
@@ -0,0 +1,19 @@
+let data=["Js is an very important language"];
+let data1="Hi User Welcome Javascript User";
+console.log(data1.startsWith("A"));
+console.log(data1.endsWith("Welcome"));
+console.log(data1.indexOf("User"));//its show from starting and show the index
+console.log(data1.lastIndexOf("User"));//its show from last and show the last index value
+
+let text1='HTML,CSS,JS';
+let arry= text1.split(",");
+console.log(arry);
+
+
+let text2="Hello";
+console.log(text2.repeat(4));
+
+let n1=new Number(1000);//if we add new it convert it into object
+console.log(n1);
+console.log(n1.valueOf());
+
