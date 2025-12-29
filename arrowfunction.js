169b2b07cfdbb42b3686f80c3498bc79424240d9
NoorainBhure <noorainbhure123@gmail.com>
2025-12-29 15:20:38 +0530

    update

---
+++ arrowfunction.js
@@ -0,0 +1,16 @@
+const sub=()=>{
+    var x=23;
+    var y=13;
+    console.log("The subtraction is :"+(x-y));
+}
+sub();
+
+
+
+
+const multi=(x,y)=>{
+   return x*y;
+
+}
+       console.log("The multiplication is :"+multi(5,6));
+
